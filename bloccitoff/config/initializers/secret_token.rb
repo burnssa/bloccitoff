c980ca6ee59bfe6a78d589245a60240319f32cea
burnssa@gmail.com <burnssa@gmail.com>
2014-08-14 19:34:36 -0700

    app created

---
+++ bloccitoff/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bloccitoff::Application.config.secret_key_base = 'c5a6ae5bd203dc985bf15272893d5c188bc605f6e9a6f8a7cd00a6435b8d99638399f20e3bb327012bf276e41d718780f019a8d98cb594f0bd65cfbfb726c1a7'
